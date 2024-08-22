d4d64d29b12185291dd36c9d885cb7be8d5d11e4
JunHo Kim <taotrpmar@gmail.com>
2024-08-22 12:00:07 +0900

    circuit_change

--- circuit/circuit2/compile.sh
+++ circuit/circuit2/compile.sh
@@ -1,3 +1,9 @@
+cd ..
+cd ..
+cd parser
+cargo run
+cd ..
+cd circuit/circuit2
 circom main.circom --r1cs --wasm --sym -l components/
 result_json_path="/mnt/c/Users/taotr/simple_zk_spa/parser/result.json"
 node main_js/generate_witness.js main_js/main.wasm "$result_json_path" witness.wtns
